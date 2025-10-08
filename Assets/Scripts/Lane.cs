393608158e78567bf97467b195f49151dfc5d015
DiegoSF9009 <diegosf9009@gmail.com>
2025-10-07 18:44:52 -0600

    Zombie y Vida

--- Assets/Scripts/Lane.cs
+++ Assets/Scripts/Lane.cs
@@ -5,6 +5,12 @@ public class Lane : MonoBehaviour
     [SerializeField]
 
     private List<Transform> frames;
+    [SerializeField]
+
+    private Transform enemySpawnPoint;
+
 
     public List<Transform> Frames => frames;
+
+    public Transform EnemySpawnPoint => enemySpawnPoint;
 }
